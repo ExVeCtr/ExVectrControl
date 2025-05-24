b2f99bc72dead0b0148b625f73505637c6a41b70
ChSt15 <chrissteffen98@gmail.com>
2025-05-24 11:30:18 +0200

    Starship flaps now do simple vehicle rate stabilisation.

--- include/ExVectrControl/starship/control_attitude_flaps.hpp
+++ include/ExVectrControl/starship/control_attitude_flaps.hpp
@@ -25,6 +25,22 @@ namespace VCTR
             bool enableFlaps = false; // Enable or disable the flap actuators
         };
 
+        struct ControlAttitudeBellyFlopSetting
+        {
+            float azimuthAngle_Rad = 0; // In what respect to north the vehicle should point to.
+            float pitchAngle_Rad = 0; // What angle to pitch the vehicle to in respect to the Body Y Axis.
+            
+            enum class BellyFlopMode : uint8_t
+            {
+                BellyFlopMode_Acsent, // Flaps are disabled and depending on the bool, either fully restracted or fully extended.
+                BellyFlopMode_Stabilize, // Stabilize the vehicle in belly flop mode. (Attempts to reach the given angles)
+                BellyFlopMode_Upright, // Help to upright the vehicle. (Top flaps are extended, bottom flaps are retracted)
+            };
+
+            BellyFlopMode bellyFlopMode = BellyFlopMode::BellyFlopMode_Acsent; // The current belly flop mode.
+
+        };
+
         /**
          * @brief Simple one dimensional PID controller.
          */
@@ -32,25 +48,39 @@ namespace VCTR
         {
         private:
 
-            Core::Simple_Subscriber<Core::Timestamped<Math::Vector<float, 7>>> attSubr_;
-
             // Setpoint for the position. In form: [V, P], where V is the linear velocity vector and P is the position vector.
-            Core::Simple_Subscriber<Math::Vector<float, 7>> stateSetpointSubr_;
-            Math::Vector<float, 7> stateSetpoint_; // Setpoint for the position. In form: [V, P], where V is the linear velocity vector and P is the position vector.
+            //Core::Simple_Subscriber<Math::Vector<float, 7>> stateSetpointSubr_;
+            //Math::Vector<float, 7> stateSetpoint_; // Setpoint for the position. In form: [V, P], where V is the linear velocity vector and P is the position vector.
             // Setpoint for the acceleration in reference frame.
 
             // Current attitude estimation. In form: [W, Q], where W is the angular velocity vector and Q is a unit quaternion rotation from the reference frame to body frame.
+            Core::Simple_Subscriber<Core::Timestamped<Math::Vector<float, 7>>> attMeasSubr_;
             Core::Timestamped<Math::Vector<float, 7>> attitudeEstimation_;
+
+            // Current position estimation. In form: [V, P] in reference frame, where V is the linear velocity vector and P is the position vector.
+            Core::Simple_Subscriber<Core::Timestamped<Math::Vector<float, 6>>> posMeasSubr_;
+            Core::Timestamped<Math::Vector<float, 6>> positionEstimation_;
+
+            Core::Simple_Subscriber<ControlAttitudeBellyFlopSetting> controlSetting_;
+
+            // Output topic for the flap settings.
+            Core::Topic_Publisher<ControlAttitudeFlapSetting> flapOutputTopic_;
 
             
 
             // Control parameters
+            float flapTopNeutralAngle_Rad_ = 50*DEGREES; // Neutral angle for the top flaps in radians.
+            float flapBottomNeutralAngle_Rad_ = 40*DEGREES; // Neutral angle for the bottom flaps in radians.
+
+            float enableThresX_ = 0.1f; // Control will enable if the velocity in Body X direction is above this threshold.
+            float enableThresAngle_Rad_ = 45*DEGREES; // Control will enable if the belly down angle is within this threshold.
+
             //float attitudeXGain_ = 1; // Attitude control gain.
             //float attitudeYGain_ = 1; // Attitude control gain.
             //float attitudeZGain_ = 1; // Attitude control gain.
-            float attitudeRateXGain_ = 1; // Attitude rate control gain.
-            float attitudeRateYGain_ = 1; // Attitude rate control gain.
-            float attitudeRateZGain_ = 1; // Attitude rate control gain.
+            float attitudeRateXGain_ = 0.2; // Attitude rate control gain.
+            float attitudeRateYGain_ = 0.5; // Attitude rate control gain.
+            float attitudeRateZGain_ = 0.1; // Attitude rate control gain.
 
             bool enableControl_ = false; // Enable or disable the control system.
 
@@ -73,11 +103,20 @@ namespace VCTR
              * * @brief Subsribes to a topic to which the attitude estimation is published in form: [W, Q], where W is the angular velocity vector and Q is a unit quaternion rotation from the reference frame to body frame.
              */
             void subscribeAttitudeMeasurement(Core::Topic<Core::Timestamped<Math::Vector<float, 7>>> &attTopic);
+            
+            /**
+             * @brief Subsribes to a topic to which the position estimation is published in form: [V, P], where V is the linear velocity vector and P is the position vector.
+             */
+            void subscribePositionMeasurement(Core::Topic<Core::Timestamped<Math::Vector<float, 6>>> &posTopic);
 
             /**
              * @brief Subsribes to a topic to which the setpoint is published in form: [V, P], where V is the linear velocity vector and P is the position vector.
              */
             //void subscribeAttitudeSetpoint(Core::Topic<Math::Vector<float, 7>> &setpointTopic);
+
+            void subscribeControlSetting(Core::Topic<ControlAttitudeBellyFlopSetting> &controlSettingTopic) {
+                controlSetting_.subscribe(controlSettingTopic);
+            }
 
             /**
              * @brief Uses the given topic to publish the flap settings for this controllers output.
--- src/starship/control_attitude_flaps.cpp
+++ src/starship/control_attitude_flaps.cpp
@@ -33,7 +33,15 @@ namespace VCTR
          */
         void ControlAttitudeFlaps::subscribeAttitudeMeasurement(Core::Topic<Core::Timestamped<Math::Vector<float, 7>>> &attTopic)
         {
-            attSubr_.subscribe(attTopic);
+            attMeasSubr_.subscribe(attTopic);
+        }
+
+        void ControlAttitudeFlaps::subscribePositionMeasurement(Core::Topic<Core::Timestamped<Math::Vector<float, 6>>> &posTopic) {
+            posMeasSubr_.subscribe(posTopic);
+        }
+
+        void ControlAttitudeFlaps::subscribeFlapSettingOutputTopic(Core::Topic<ControlAttitudeFlapSetting> &flapOutputTopic){
+            flapOutputTopic_.subscribe(flapOutputTopic);
         }
 
         void ControlAttitudeFlaps::taskCheck() {
@@ -43,17 +51,68 @@ namespace VCTR
         void ControlAttitudeFlaps::taskInit() {
 
             //Only run when all subscribers have gotten data at least once
-            if (!attSubr_.isDataNew() || !stateSetpointSubr_.isDataNew()) {
+            if (!attMeasSubr_.isDataNew()/* || !stateSetpointSubr_.isDataNew()*/) {
                 setInitialised(false);
             }
 
+            attitudeEstimation_.data = {0, 0, 0, 1, 0, 0, 0};
             
-
         }
 
         void ControlAttitudeFlaps::taskThread() {
 
-            
+            //if (stateSetpointSubr_.isDataNew()) {
+            //    stateSetpoint_ = stateSetpointSubr_.getItem();
+            //}
+            if (posMeasSubr_.isDataNew()) {
+                positionEstimation_ = posMeasSubr_.getItem();
+            }
+            if (attMeasSubr_.isDataNew()) {
+                attitudeEstimation_ = attMeasSubr_.getItem();
+            }
+
+            //Retrieve the latest data from the state vectors
+            //Math::Vector_F position = positionEstimation_.data.block<3, 1>(3, 0);
+            Math::Vector_F velocity = positionEstimation_.data.block<3, 1>(0, 0);
+            Math::Quat_F attitude = attitudeEstimation_.data.block<4, 1>(3, 0);
+            auto angularVelocity = attitudeEstimation_.data.block<3, 1>(0, 0);
+
+            // Determin of the control should be enabled or not
+            //We want to only enalbe the flaps if the velocity in the x direction is above a certain threshold and ZY is below a certain threshold
+            auto bodyXAxis = attitude.conjugate().rotate(Math::Vector_F({1, 0, 0})); // Get the body X-axis in reference frame
+            auto velXAxis = velocity.getProjectionOn(bodyXAxis); // Project the velocity onto the body X-axis plane
+            auto bellyDownAngle = bodyXAxis.getAngleTo(Math::Vector_F({0, 0, 1})); // Calculate the angle between the body X-axis and the Z-axis
+            // Scale the flap factor so if over threshold, then begin reducing factor until 10 deg over the threshold, then disable flaps be setting factor 0
+            float flapFactor = (bellyDownAngle - enableThresAngle_Rad_)/(10*DEGREES); // Calculate the flap factor based on the belly down angle
+            if (flapFactor < 0) flapFactor = 0; // If the belly down angle is below the threshold, set the flap factor to 0
+            if (flapFactor > 1) flapFactor = 1; // If the belly down angle is above the threshold, set the flap factor to 1
+            flapFactor = 1 - flapFactor;
+            //LOG_MSG("Belly down angle: %.2f, thres: %.2f, flap factor: %.2f\n", bellyDownAngle / DEGREES, enableThresAngle_Rad_ / DEGREES, flapFactor); // Print the belly down angle and flap factor to the console
+
+            // Calculate the controller output
+            Math::Vector<float, 3> attRateCtrlOutput({
+                -(angularVelocity(0)) * attitudeRateXGain_,
+                -(angularVelocity(1)) * attitudeRateYGain_,
+                -(angularVelocity(2)) * attitudeRateZGain_,
+            });
+
+
+            //Output to flap mapping
+            ControlAttitudeFlapSetting flapSetting;
+            flapSetting.flapTLAngle_Rad = flapTopNeutralAngle_Rad_ - attRateCtrlOutput(0) - attRateCtrlOutput(1) + attRateCtrlOutput(2);
+            flapSetting.flapTRAngle_Rad = flapTopNeutralAngle_Rad_ + attRateCtrlOutput(0) - attRateCtrlOutput(1) - attRateCtrlOutput(2);
+            flapSetting.flapBLAngle_Rad = flapBottomNeutralAngle_Rad_ + attRateCtrlOutput(0) + attRateCtrlOutput(1) + attRateCtrlOutput(2);
+            flapSetting.flapBRAngle_Rad = flapBottomNeutralAngle_Rad_ - attRateCtrlOutput(0) + attRateCtrlOutput(1) - attRateCtrlOutput(2);
+            flapSetting.enableFlaps = true;//enableControl_; // Enable or disable the flap actuators
+
+            // LERP the flap angles based on the flap factor
+            flapSetting.flapTLAngle_Rad = flapSetting.flapTLAngle_Rad * flapFactor + 90*DEGREES * (1 - flapFactor); // Scale the flap angle based on the flap factor
+            flapSetting.flapTRAngle_Rad = flapSetting.flapTRAngle_Rad * flapFactor + 90*DEGREES * (1 - flapFactor); // Scale the flap angle based on the flap factor
+            flapSetting.flapBLAngle_Rad = flapSetting.flapBLAngle_Rad * flapFactor + 90*DEGREES * (1 - flapFactor); // Scale the flap angle based on the flap factor
+            flapSetting.flapBRAngle_Rad = flapSetting.flapBRAngle_Rad * flapFactor + 90*DEGREES * (1 - flapFactor); // Scale the flap angle based on the flap factor
+
+            //Publish the flap setting to the topic
+            flapOutputTopic_.publish(flapSetting);
 
         }
 
